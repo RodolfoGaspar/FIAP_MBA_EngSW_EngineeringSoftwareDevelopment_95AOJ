ea7f43caa88b4b4fdd8bb2ebd5d3d2a9475536bd
gabriel.chiode <gabrielchiode@hotmail.com>
2025-03-28 22:43:26 -0300

    add cors

--- VagasAPI/Program.cs
+++ VagasAPI/Program.cs
@@ -7,15 +7,32 @@ builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configuração do CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirTudo", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Habilita o CORS na aplicação
+app.UseCors("PermitirTudo");
+
 app.MapGet("/v1/vagas", (AppDbContext context) =>
 {
-    var vagas = context.Vagas;
-    return vagas is not null ? Results.Ok(vagas) : Results.NotFound();
-}).Produces<Vaga>();
+    var vagas = context.Vagas?.ToList();
+    return vagas is not null
+        ? Results.Ok(new { vagas })
+        : Results.NotFound();
+}).Produces<object>();
 
 app.MapGet("/v1/vagas/{id}", (string id, AppDbContext context) =>
 {
@@ -25,14 +42,15 @@ app.MapGet("/v1/vagas/{id}", (string id, AppDbContext context) =>
         return vagas is not null ? Results.Ok(vagas) : Results.NotFound();
     }
     return Results.NotFound();
-
 }).Produces<Vaga>();
 
 app.MapPost("/v1/vagas", (AppDbContext context, CreateVagaViewModel model) =>
 {
     var vaga = model.MapTo();
     if (!model.IsValid)
-    { return Results.BadRequest(model.Notifications); }
+    {
+        return Results.BadRequest(model.Notifications);
+    }
 
     context?.Vagas?.Add(vaga);
     context?.SaveChanges();
@@ -44,7 +62,9 @@ app.MapPut("/v1/vagas", (AppDbContext context, AlterVagaViewModel model) =>
 {
     var modelVaga = model.MapTo();
     if (!model.IsValid)
-    { return Results.BadRequest(model.Notifications); }
+    {
+        return Results.BadRequest(model.Notifications);
+    }
 
     var vaga = context?.Vagas?.FirstOrDefault(v => v.Id == model.Id);
 
@@ -59,19 +79,23 @@ app.MapPut("/v1/vagas", (AppDbContext context, AlterVagaViewModel model) =>
         return Results.Created($"/v1/vagas/{modelVaga.Id}", modelVaga);
     }
 
-    return Results.NoContent();    
+    return Results.NoContent();
 });
 
 app.MapGet("/v1/vagas/status", (AppDbContext context) =>
 {
-    return Enum.GetValues(typeof(StatusVagaEnum)).Cast<StatusVagaEnum>().Select(s => new { Id = s, Name = Enum.GetName(s) }).ToList();
-
+    return Enum.GetValues(typeof(StatusVagaEnum))
+               .Cast<StatusVagaEnum>()
+               .Select(s => new { Id = s, Name = Enum.GetName(s) })
+               .ToList();
 }).Produces<dynamic>();
 
 app.MapGet("/v1/vagas/tipos", (AppDbContext context) =>
 {
-    return Enum.GetValues(typeof(TipoVagaEnum)).Cast<TipoVagaEnum>().Select(s => new { Id = s, Name = Enum.GetName(s) }).ToList();
-
+    return Enum.GetValues(typeof(TipoVagaEnum))
+               .Cast<TipoVagaEnum>()
+               .Select(s => new { Id = s, Name = Enum.GetName(s) })
+               .ToList();
 }).Produces<dynamic>();
 
 app.Run();
