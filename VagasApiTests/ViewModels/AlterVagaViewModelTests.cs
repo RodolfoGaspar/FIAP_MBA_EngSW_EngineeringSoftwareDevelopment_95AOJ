7a6221be29b31235a38f6d12e1453cb9627c6c56
Rodolfo Gaspar <dolfogaspar@gmail.com>
2025-03-29 21:25:49 -0300

    feat(vagas): adicionado testes unitários à API de Vagas

---
+++ VagasApiTests/ViewModels/AlterVagaViewModelTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VagasApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VagasApi.ViewModels.Tests
+{
+    [TestClass()]
+    public class AlterVagaViewModelTests
+    {
+        [TestMethod()]
+        public void AlterVaga_OK()
+        {
+            AlterVagaViewModel model = new AlterVagaViewModel()
+            {
+                IdEstacionamento = Guid.NewGuid(),
+                Status = StatusVagaEnum.DISPONIVEL,
+                TipoVaga = TipoVagaEnum.COBERTA,
+                ValorHora = 10
+            };
+            var vaga = model.MapTo();
+
+            Assert.IsTrue(model.IsValid);
+        }
+
+        [TestMethod()]
+        public void CreateVaga_ErroValor()
+        {
+            AlterVagaViewModel model = new AlterVagaViewModel()
+            {
+                IdEstacionamento = Guid.NewGuid(),
+                Status = StatusVagaEnum.DISPONIVEL,
+                TipoVaga = TipoVagaEnum.COBERTA,
+                ValorHora = 0
+            };
+            var vaga = model.MapTo();
+
+            Assert.IsFalse(model.IsValid);
+        }
+
+        [TestMethod()]
+        public void CreateVaga_ErroStatus()
+        {
+            AlterVagaViewModel model = new AlterVagaViewModel()
+            {
+                IdEstacionamento = Guid.NewGuid(),
+                Status = (StatusVagaEnum)11,
+                TipoVaga = TipoVagaEnum.COBERTA,
+                ValorHora = 10
+            };
+            var vaga = model.MapTo();
+
+            Assert.IsFalse(model.IsValid);
+        }
+
+        [TestMethod()]
+        public void CreateVaga_ErroTipoVaga()
+        {
+            AlterVagaViewModel model = new AlterVagaViewModel()
+            {
+                IdEstacionamento = Guid.NewGuid(),
+                Status = StatusVagaEnum.DISPONIVEL,
+                TipoVaga = (TipoVagaEnum)11,
+                ValorHora = 10
+            };
+            var vaga = model.MapTo();
+
+            Assert.IsFalse(model.IsValid);
+        }
+    }
+}
---
+++ VagasApiTests/ViewModels/CreateVagaViewModelTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VagasApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VagasApi.ViewModels.Tests
+{
+    [TestClass()]
+    public class CreateVagaViewModelTests
+    {
+        [TestMethod()]
+        public void CreateVaga_OK()
+        {
+            CreateVagaViewModel model = new CreateVagaViewModel()
+            {
+                IdEstacionamento = Guid.NewGuid(),
+                Status = StatusVagaEnum.DISPONIVEL,
+                TipoVaga = TipoVagaEnum.COBERTA,
+                ValorHora = 10
+            };
+            var vaga = model.MapTo();
+
+            Assert.IsTrue(model.IsValid);
+        }
+
+        [TestMethod()]
+        public void CreateVaga_ErroValor()
+        {
+            CreateVagaViewModel model = new CreateVagaViewModel()
+            {
+                IdEstacionamento = Guid.NewGuid(),
+                Status = StatusVagaEnum.DISPONIVEL,
+                TipoVaga = TipoVagaEnum.COBERTA,
+                ValorHora = 0
+            };
+            var vaga = model.MapTo();
+
+            Assert.IsFalse(model.IsValid);
+        }
+
+        [TestMethod()]
+        public void CreateVaga_ErroStatus()
+        {
+            CreateVagaViewModel model = new CreateVagaViewModel()
+            {
+                IdEstacionamento = Guid.NewGuid(),
+                Status = (StatusVagaEnum)11,
+                TipoVaga = TipoVagaEnum.COBERTA,
+                ValorHora = 10
+            };
+            var vaga = model.MapTo();
+
+            Assert.IsFalse(model.IsValid);
+        }
+
+        [TestMethod()]
+        public void CreateVaga_ErroTipoVaga()
+        {
+            CreateVagaViewModel model = new CreateVagaViewModel()
+            {
+                IdEstacionamento = Guid.NewGuid(),
+                Status = StatusVagaEnum.DISPONIVEL,
+                TipoVaga = (TipoVagaEnum)11,
+                ValorHora = 10
+            };
+            var vaga = model.MapTo();
+
+            Assert.IsFalse(model.IsValid);
+        }
+    }
+}
